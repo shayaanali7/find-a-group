949581a7fd15e9fae0f78f0435799a6c147fdbe4
Muhammad Shayaan Ali <shayaanali7@gmail.com>
2025-06-15 22:35:03 +0300

    Changing logo file name

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
       <main className="min-h-screen bg-gradient-to-br from-purple-950 via-purple-800 to-purple-700 flex items-center justify-center">
         <div className="flex space-x-">
           <div>
-            <Image src="/assets/logo.jpg" width={500} height={500} alt="logo" className="object-contain"/>
+            <Image src="/assets/logo.png" width={500} height={500} alt="logo" className="object-contain"/>
           </div>
           
           <div className="bg-white p-6 rounded-lg">
