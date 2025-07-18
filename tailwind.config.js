42ad8a9f26eac7e5d413aeb5bcef928eaae14cda
Muhammad Shayaan Ali <shayaanali7@gmail.com>
2025-07-19 00:36:21 +0300

    More Vercel Deployment Issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
     safelist: [
         'hover:bg-purple-500',
         'hover:bg-blue-500',
-        'hover:bg-emerald-500'
+        'hover:bg-emerald-500',
+        'hover:bg-yellow-500',
+        'hover:bg-orange-500',
+        'hover:bg-pink-500',
+        'hover:bg-green-500',
     ]
 }
