127514beb56d24ec894f9d2cdc35e33bc2945cc2
Muhammad Shayaan Ali <shayaanali7@gmail.com>
2025-08-22 09:23:44 +0300

    Removed useRouter import

--- app/resetPassword/page.tsx
+++ app/resetPassword/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import ModalScreen from '../components/ModalScreen'
 import Link from 'next/link'
 import { useResetPassword } from './useResetPassword'
